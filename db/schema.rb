f19b1e225bd75edee9928c63f1142da3cb683235
req12345 <req12345@mail.ru>
2021-05-24 13:54:16 +0300

    add NULL constraints, level to tests, correct to answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_203337) do
+ActiveRecord::Schema.define(version: 2021_05_24_102559) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_203337) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
