c4b0d6ecf8ce128937d4864eeaeda0229bbcdba8
req12345 <req12345@mail.ru>
2021-05-25 11:09:20 +0300

    fix syntax

--- db/migrate/20210523200918_create_tests.rb
+++ db/migrate/20210523200918_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, :default => 1
+      t.integer :level, default: 1
       t.references :category, foreign_key: true
 
       t.timestamps
