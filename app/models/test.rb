a8ccc79ab646964d11b0af5c99922561eb02fe9a
req12345 <req12345@mail.ru>
2021-05-29 16:50:20 +0300

    [add] associations Task-5 (#6)
    * [add] associations Task-5
    
    * add refences from tests to user

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :users_tests
+  has_many :users, through: :users_tests
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
   def self.by_category(category)
       joins('INNER JOIN categories ON tests.category_id = categories.id')
       .where('categories.title = ?', category).order(id: :desc).pluck('tests.title')
