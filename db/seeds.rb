b7783d7343db8c7c0e2f181af83cc8c31969de93
Ivan Bakhmetov <req12345@mail.ru>
2021-05-29 17:43:39 +0300

    [TES-2] add scope correct_answer

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,11 +32,11 @@ question = Question.create([
   {body: "What month and year the 'HTML' was invented?", test_id: test[4].id}])
 
 answer = Answer.create([
-  {body: 'february 1993', question_id: question[0].id},
+  {body: 'february 1993', question_id: question[0].id, correct: true},
   {body: 'october 1985', question_id: question[1].id},
-  {body: 'february 1991', question_id: question[2].id},
+  {body: 'february 1991', question_id: question[2].id, correct: true},
   {body: 'december 1995', question_id: question[3].id},
-  {body: 'december 1996', question_id: question[4].id}])
+  {body: 'december 1996', question_id: question[4].id, correct: true}])
 
 user_test = UsersTest.create([
   {user_id: user[0].id, test_id: test[0].id},
