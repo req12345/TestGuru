a83a97914909ca4e081211521e485d7e12134bc6
Ivan Bakhmetov <req12345@mail.ru>
2021-05-30 18:04:48 +0300

    [TES-9] refactoring tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
   validates :name, presence: true
-  
+
   def tests_by_level(level)
-    Test.joins('INNER JOIN users_tests ON users_tests.test_id = tests.id')
-        .where('tests.level = ?', level)
-        .where(users_tests: { user_id: self.id })
+    tests.where(level: level)
   end
 end
