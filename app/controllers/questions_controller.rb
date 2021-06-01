d8df4e37ad35cd68414aa8931f4a6d95a1fa55c5
Ivan Bakhmetov <req12345@mail.ru>
2021-06-01 23:25:47 +0300

    [TES 16,17] add rescue, destroy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[create]
-  before_action :find_question, only: %i[show, destroy]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     test = Test.find_by(id: params[:test_id])
@@ -23,6 +25,10 @@ class QuestionsController < ApplicationController
      render inline: '<p>Question № <%= @question.id %>. "<%= @question.body %>" created<p>'
    end
 
+   def destroy
+     @question.destroy
+   end
+
    private
 
    def find_test
@@ -35,5 +41,9 @@ class QuestionsController < ApplicationController
 
    def question_params
      params.require(:question).permit(:body)
+   end
+
+   def rescue_with_question_not_found
+     render plain: 'Question not found'
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   resources :tests do
     resources :questions, shallow: true
   end
+
+  # get '/questions/:id/destroy', to: 'questions#destroy'
+
 end
