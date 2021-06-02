f2c15eb9a8076d210702017a079b4d5c7fd3cfe5
Ivan Bakhmetov <req12345@mail.ru>
2021-06-02 12:32:19 +0300

    [TES 11-17] fixes, rubocop checked

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,8 +1,8 @@
 class TestsController < ApplicationController
   def index
     tests = Test.all
-    tests_html = ""
-    tests_html << "<h1>All tests:</h1>"
+    tests_html = ''
+    tests_html << '<h1>All tests:</h1>'
     tests.each { |t| tests_html << "<p>#{t.title}</p>" }
     render inline: tests_html
   end
