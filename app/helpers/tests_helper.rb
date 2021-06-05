9818d91bca166a85470c973ac756cbb64cf48543
Ivan Bakhmetov <req12345@mail.ru>
2021-06-05 14:17:02 +0300

    Add Test views

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,21 @@
 module TestsHelper
+# TEST_LEVELS = { (0..1) => :easy. (2..4) => :middle, (5..6) => hard}.freeze
+
+  def test_level(test)
+    # TEST_LEVELS[test.level] || :hero
+    case test.level
+    when 1 then "easy"
+    when 2..4 then "middle"
+    when 5..6 then "hard"
+    else "hero"
+    end
+  end
+
+  def test_header(test)
+    if test.new_record?
+      "Create new test"
+    else
+      "Edit test"
+    end
+  end
 end
