58438da2a1d7d9227845db921e9f64fa34928975
Ivan Bakhmetov <req12345@mail.ru>
2021-06-10 21:56:58 +0300

    [fix] create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = user.new((user_params))
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to tests_path
