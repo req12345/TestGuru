739d49e26355871a58c357d0e9698f9c5a9768dd
Ivan Bakhmetov <req12345@mail.ru>
2021-06-10 22:24:46 +0300

    fix column name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+
+  attr_reader :password
+  attr_writer :password_confirmation
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
@@ -15,8 +19,17 @@ class User < ApplicationRecord
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 
-  def authenticate(password_swtring)
+  def authenticate(password_string)
     digest(password_string) == self.password_digest ? self : false
+  end
+
+  def password=(password_string)
+    if password_string.nil?
+      self.password_digest = nil
+    elsif password_string.present?
+      @password = password_string
+      self.password_digest = digest(password_string)
+    end
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_174600) do
+ActiveRecord::Schema.define(version: 2021_06_10_192210) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_174600) do
     t.integer "skill"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "pasword_digest"
+    t.string "password_digest"
     t.string "email", default: "", null: false
   end
 
