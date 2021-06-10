c8e51cf6ed88dd2f86bc024418ba27e3d9ed0670
Ivan Bakhmetov <req12345@mail.ru>
2021-06-10 21:04:39 +0300

    add to user authenticate & digest, gem digest/sha1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
@@ -11,5 +13,15 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
+  def authenticate(password_swtring)
+    digest(password_string) == self.password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
