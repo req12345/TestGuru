468143ea34dfe031380400d18d9cec9ed594d9dc
Ivan Bakhmetov <req12345@mail.ru>
2021-06-11 21:42:27 +0300

    add controller sessions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'tests#index'
 
   resources :users, only: :create
+  resources :sessions, only: :create
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
 
   resources :tests do
     resources :questions, shallow: true
