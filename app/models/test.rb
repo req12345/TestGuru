2dbb791f5ce6f1423d7fbf5ff183da9e9f40cdf5
Ivan Bakhmetov <req12345@mail.ru>
2021-06-12 14:53:00 +0300

    style fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,10 +5,10 @@ class Test < ApplicationRecord
   has_many :users, through: :test_passages
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-  scope :easy, -> { where(level: 0..1) }
-  scope :middle, -> { where(level: 2..4) }
-  scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :by_category, -> (category) { joins(:category).where(categories: { title: category }) }
+  scope :easy, ->{ where(level: 0..1) }
+  scope :middle, ->{ where(level: 2..4) }
+  scope :hard, ->{ where(level: 5..Float::INFINITY) }
+  scope :by_category, ->(category) { joins(:category).where(categories: { title: category }) }
 
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
