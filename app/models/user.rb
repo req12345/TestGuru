eba5568ca6695e9b75f91b06f8c73e326c20c44c
Ivan Bakhmetov <req12345@mail.ru>
2021-06-14 11:04:07 +0300

    [TES-34] installed gem Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
