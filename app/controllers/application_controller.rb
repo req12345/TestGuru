e6e2a7eef64853e6a5b0cc03f4de55ad32ba7afc
Ivan Bakhmetov <req12345@mail.ru>
2021-06-24 00:28:55 +0300

    add admin? to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user,
-                :logged_in?
 
   def after_sign_in_path_for(resource)
-     resource.is_a?(Admin) ? admin_tests_path : root_path
+     resource.admin? ? admin_tests_path : root_path
   end
 end
