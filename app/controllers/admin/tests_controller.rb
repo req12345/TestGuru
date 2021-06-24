02b540d81baf6ac42d5ff10756c8d260fa374cc8
Ivan Bakhmetov <req12345@mail.ru>
2021-06-24 21:58:10 +0300

    minor fix test_params permit

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -45,6 +45,6 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
+    params.require(:test).permit(:title, :level, :category_id)
   end
 end
