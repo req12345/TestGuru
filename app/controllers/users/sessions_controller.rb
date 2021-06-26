b5eaaf3671fcc24113e31d0d63a1a72dc5423646
Ivan Bakhmetov <req12345@mail.ru>
2021-06-27 00:48:12 +0300

    TES-35/ADD locales

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def create
     super
-    flash[:notice] = "Hello, #{current_user.first_name}!"
+    flash[:notice] = t('shared.flash.hello', user_first_name: current_user.first_name)
   end
 end
