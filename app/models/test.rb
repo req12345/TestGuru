28a9460444b4eb62c5586dca81bb344472fb44e4
Ivan Bakhmetov <req12345@mail.ru>
2021-06-30 21:35:59 +0300

    [BUG] fixed delete of test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many :test_passages
   has_many :users, through: :test_passages
   belongs_to :author, class_name: 'User', foreign_key: :author_id
