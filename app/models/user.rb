e339b69507ca6d8e37a9fa8322309441c0b541f2
Ivan Bakhmetov <req12345@mail.ru>
2021-07-04 19:57:22 +0300

    add model Gist vis migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :gists
 
   validates :email, presence: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP },
