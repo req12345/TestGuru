fb0a40e8936998e2ec8f4b688e7514c45e177fbf
Ivan Bakhmetov <req12345@mail.ru>
2021-07-04 18:22:57 +0300

    use Octokit instead Faraday

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -15,6 +15,7 @@ class GistQuestionService
   def gist_params
     {
       description: "A question about #{@test.title} from TestGuru",
+      "public": true,
       files: {
         'test-guru-question.txt' => {
           content: gist_content
--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,23 +1,18 @@
 require 'dotenv/load'
 class GitHubClient
-
-  ROOT_ENDPOINT = 'https://api.github.com'
-
+  attr_reader @http_client
+  
   def initialize
     @http_client = setup_http_client
   end
 
   def create_gist(params)
-    @http_client.post('gists') do |request|
-      request.headers['Authorization'] = "token #{ENV["GIT_HUB_ACCESS_TOKEN"]}"
-      request.headers['Content-Type'] = 'application/json'
-      request.body = params.to_json
-    end
+    @http_client.create_gist(params)
   end
 
   private
 
   def setup_http_client
-    Faraday.new(url: ROOT_ENDPOINT)
+    Octokit::Client.new(access_token: "#{ENV["GIT_HUB_ACCESS_TOKEN"]}")
   end
 end
