b531bf98aee711dec82a10088d75cb244f28f86e
Ivan Bakhmetov <req12345@mail.ru>
2021-07-08 21:49:46 +0300

    add sorting by title

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+//app/javascript/utilities/sorting
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "utilities/sorting"
+import "utilities/password"
 
 Rails.start()
 Turbolinks.start()
