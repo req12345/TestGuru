c3f4cd49276ceda229a946a4a76bb006d46453be
Ivan Bakhmetov <req12345@mail.ru>
2021-07-12 21:50:27 +0300

    add progress bar

---
+++ app/javascript/utilities/progress_bar.js
@@ -0,0 +1,19 @@
+document.addEventListener('turbolinks:load', function () {
+    currentProgress()
+
+    let control = document.querySelector('.submit')
+
+    if (control) {
+      control.addEventListener('click', currentProgress)
+    }
+})
+
+function currentProgress(event) {
+    let current_question = document.querySelector('.bar').dataset.currentNumber - 1
+    let total_questions = document.querySelector('.bar').dataset.totalQuestions
+    let result_percentage = ((100 / total_questions).toFixed(2)) * current_question
+    let item = document.querySelector('.progress-bar');
+
+    item.style.width = result_percentage + "%";
+    item.innerHTML = result_percentage + "%";
+}
