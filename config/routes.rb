3e7387eb20d7d6ef718b1ea50bbda1d245186e7c
req12345 <req12345@mail.ru>
2021-07-13 20:35:39 +0300

    Feature/data attributes & progress indicator (#18)
    * add edit inline form
    
    * add div progress bar with data
    
    * add def total_questions
    
    * add progress bar
    
    * remove jQuery code
    
    * style fix
    
    * add sorting tests for user,
    
    * use jQuery instead JS
    
    * fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :gists, only: :index, shallow: true
     resources :tests do
+      patch :update_inline, on: :member
+
       resources :questions, except: :index, shallow: true do
         resources :answers, except: :index, shallow: true
       end
