f9b98247074706563035a50204a8202e20c8f985
Ivan Bakhmetov <req12345@mail.ru>
2021-07-18 00:13:40 +0300

    generate model Feedback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_145220) do
+ActiveRecord::Schema.define(version: 2021_07_17_210511) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -23,6 +26,15 @@ ActiveRecord::Schema.define(version: 2021_07_04_145220) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email", null: false
+    t.string "header", null: false
+    t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
