4501317c2052e0288aeac47a30e69c936ff65398
Ivan Bakhmetov <req12345@mail.ru>
2021-07-24 19:13:30 +0300

    dd scope animals

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,9 @@ class Category < ApplicationRecord
   has_many :tests
 
   default_scope { order(title: :asc) }
+  scope :animals, ->{ where(title: 'Animals') }
 
   validates :title, presence: true
+
+
 end
