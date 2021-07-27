b99c8d787baac2d4ed3bee5699a5768b8fdadaa8
Ivan Bakhmetov <req12345@mail.ru>
2021-07-27 23:33:27 +0300

    style fixes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,6 @@ class Category < ApplicationRecord
   has_many :tests
 
   default_scope { order(title: :asc) }
-  scope :animals, ->{ where(title: 'Animals') }
 
   validates :title, presence: true
 end
