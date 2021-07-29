d99234ad73260694944e22ef41467883ee944ff8
Ivan Bakhmetov <req12345@mail.ru>
2021-07-30 01:11:15 +0300

    fix bug creating answers add form_correct, correct routes

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -1,12 +1,11 @@
 class Admin::AnswersController < ApplicationController
-  before_action :find_question, only: %i[new create destroy]
-  before_action :find_answer, only: %i[show edit update destroy]
+  before_action :find_question, only: %i[create]
+  before_action :find_answer, only: %i[edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_answer_not_found
 
-  def show; end
-
   def new
+    @question = Question.find_by(id: params[:format])
     @answer = @question.answers.new
   end
 
@@ -14,7 +13,7 @@ class Admin::AnswersController < ApplicationController
 
   def update
     if @answer.update(answer_params)
-      redirect_to [:admin, @answer]
+      redirect_to [:admin, @answer.question]
     else
       render :edit
     end
@@ -37,18 +36,18 @@ class Admin::AnswersController < ApplicationController
   private
 
   def find_question
-    @question = Question.find(params[:question_id])
+    @question = Question.find_by(id: params[:question_id])
   end
 
   def find_answer
-    @answer = Answer.find(params[:id])
+    @answer = Answer.find_by(id: params[:id])
   end
 
   def answer_params
-    params.require(:answer).permit(:body)
+    params.require(:answer).permit(:body, :correct)
   end
 
-  def rescue_with_question_not_found
+  def rescue_with_answer_not_found
     render plain: 'Answer not found'
   end
 end
